cfc6227f03ac1d99a6f6b9520d24e13c83b7fe54
Belal khan <belalkhan.dev@gmail.com>
2021-09-08 19:08:52 +0600

    changes in design

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,20 @@
         $(document).on('click', '#nav-control', function() {
             $('main').toggleClass('mini-nav');
         });
+
+        //active navigation class
+        var current = location.pathname;
+        $('.navigation-menu ul li a').each(function () {
+            var $this = $(this);
+             if (current == '/' || current == '/admin' || current == '/admin/') {
+                 $('.index-link').closest('li').addClass('active');
+                 return false;
+             }else if ($this.attr('href').indexOf(current) !== -1) {
+                $this.closest('.treeview').addClass('nav-open');
+                $this.closest('.treeview-menu').show();
+                $this.closest('li').addClass('active');
+            }
+        });
         
     });
 
